b0494fb17f4bdea8288e7ae14965fec5db7a7c14
Karen Ho <littlepinkrose12@gmail.com>
2017-10-21 16:31:53 -0700

    adding dynamic lists

---
+++ Assets/ItemList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList : MonoBehaviour {
+
+    public GameObject itemEntryPrefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 2; i++) {
+            GameObject go = (GameObject)Instantiate(itemEntryPrefab);
+            go.transform.SetParent(this.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ShoppingListManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingListManager : MonoBehaviour {
+
+    List<string> shoppingList;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(GetItem(0));
+	}
+
+    void Init() {
+        if (shoppingList != null) {
+            return;
+        }
+        shoppingList = new List<string>();
+        shoppingList.Add("coffee");
+        shoppingList.Add("bananas");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string GetItem(int index) {
+        Init();
+
+        return shoppingList[index];
+    }
+}
