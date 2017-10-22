27a62758f56457c747ed6cb1e45e8e75f9ecd243
yosun <yosun@nusoy.com>
2017-10-21 23:50:02 -0700

    ui tweaks
    faded bottom strip
    shop button shakes

---
+++ Assets/UI/UIShaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class UIShaker : MonoBehaviour {
+ 
+	void Start () {
+		Shake ();
+	}
+
+	void Shake(){
+		GetComponent<RectTransform> ().DOShakePosition (Random.Range (1.37f, 3.14f),20f).OnComplete (Shake);
+	}
+	  
+}
